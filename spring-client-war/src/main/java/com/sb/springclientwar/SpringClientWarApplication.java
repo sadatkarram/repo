dc57443e8435ee9ad95a3e1a9928811df04b237f
unknown <sadatkarram@gmail.com>
2020-06-06 15:16:00 +0530

    Adding new project spring-client-war

---
+++ spring-client-war/src/main/java/com/sb/springclientwar/SpringClientWarApplication.java
@@ -0,0 +1,13 @@
+package com.sb.springclientwar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClientWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringClientWarApplication.class, args);
+	}
+
+}
---
+++ spring-client-war/src/test/java/com/sb/springclientwar/SpringClientWarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.springclientwar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringClientWarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
